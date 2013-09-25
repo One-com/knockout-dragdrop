f09bacbfc808a8358932c162b73bcdd4d49908c7
Sune Simonsen <sss@one.com>
2013-09-25 15:43:10 +0200

    Added drag styling example

--- example/main.js
+++ example/main.js
@@ -36,6 +36,30 @@
                 model.target.remove(data);
                 model.source.push(data);
             }
+        },
+
+        styling: {
+            source: ko.observableArray(ko.utils.arrayMap(names, function (name) {
+                return {
+                    value: name,
+                    dragging: ko.observable(false)
+                };
+            })),
+            target: ko.observableArray(),
+            dragStart: function (item) {
+                item.dragging(true);
+            },
+            dragEnd: function (item) {
+                item.dragging(false);
+            },
+            dropFromSource: function (data, model) {
+                model.source.remove(data);
+                model.target.push(data);
+            },
+            dropFromTarget: function (data, model) {
+                model.target.remove(data);
+                model.source.push(data);
+            }
         }
     };
     ko.applyBindings(model, $('.demo')[0]);
