c847dd48f4517acb2c77feb58a595176ed22af86
Sune Simonsen <sss@one.com>
2013-09-25 15:25:14 +0200

    Added drag and drop example with custom drag element

--- example/main.js
+++ example/main.js
@@ -1,24 +1,41 @@
 /*global $, ko*/
 (function ($, ko) {
+    var names = [
+        'Declan',
+        'Tessa',
+        'Claire',
+        'Violet',
+        'Alice',
+        'Mia',
+        'Camille',
+        'Aiden'
+    ];
+
     var model = {
-        source: ko.observableArray([
-			'Declan',
-			'Tessa',
-			'Claire',
-			'Violet',
-			'Alice',
-			'Mia',
-			'Camille',
-			'Aiden'
-        ]),
-        target: ko.observableArray(),
-        dropFromSource: function (data, model) {
-            model.source.remove(data);
-            model.target.push(data);
+        simple: {
+            source: ko.observableArray(names),
+            target: ko.observableArray(),
+            dropFromSource: function (data, model) {
+                model.source.remove(data);
+                model.target.push(data);
+            },
+            dropFromTarget: function (data, model) {
+                model.target.remove(data);
+                model.source.push(data);
+            }
         },
-        dropFromTarget: function (data, model) {
-            model.target.remove(data);
-            model.source.push(data);
+
+        dragElement: {
+            source: ko.observableArray(names),
+            target: ko.observableArray(),
+            dropFromSource: function (data, model) {
+                model.source.remove(data);
+                model.target.push(data);
+            },
+            dropFromTarget: function (data, model) {
+                model.target.remove(data);
+                model.source.push(data);
+            }
         }
     };
     ko.applyBindings(model, $('.demo')[0]);
