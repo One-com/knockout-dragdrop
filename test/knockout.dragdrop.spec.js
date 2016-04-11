6cf4bba42bba000721ca928cea34d1888cabe30e
Maarten Winter <mw@one.com>
2016-04-11 17:46:29 +0200

    Convert tabs to spaces

--- test/knockout.dragdrop.spec.js
+++ test/knockout.dragdrop.spec.js
@@ -1,7 +1,7 @@
 var expect = window.weknowhow.expect;
 
 describe('knockout.dragdrop', function () {
-	it('attaches the binding handlers', function () {
-		expect(ko.bindingHandlers, 'to have keys', ['dropZone', 'dragEvents', 'dragZone', 'scrollableOnDragOver']);
-	});
+    it('attaches the binding handlers', function () {
+        expect(ko.bindingHandlers, 'to have keys', ['dropZone', 'dragEvents', 'dragZone', 'scrollableOnDragOver']);
+    });
 });
